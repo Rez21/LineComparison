0bf03b7dc1bc84b74edba81a02c9af664be73ad9
Rez21 <banprasad21@gmail.com>
2022-08-13 20:27:27 +0530

    [Raghav] Add: UC1 Line length calculation

---
+++ LineComparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_Line_Length length = new UC1_Line_Length();
+            length.CalLength(5, 6, 7, 8);
+        }
+    }
+}
